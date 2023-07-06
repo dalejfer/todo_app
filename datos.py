b129706c014b7614278fb3c85fd041d676c75e08
Daniel Fernandez <daniel_alejandro_fernandez@hotmail.com>
2023-07-06 17:21:54 -0300

    algunos avances

--- datos.py
+++ datos.py
@@ -3,24 +3,39 @@ import mysql.connector
 config = {'user': 'juan',
           'password': '123456',
           'host': '127.0.0.1',
-          'database': 'cine'}
+          'database': 'todo'}
 
-class Datos:
-    def __init__(self):
-        self.conn = mysql.connector.connect(**config)
-        self.cur = self.conn.cursor()
+def conectar():
+    conn = mysql.connector.connect(**config)
+    return conn
 
-    def create_database(self):
-        pass
+# def create_database(self):
+#     pass            
 
-    def get_tasks(self):
-        pass
+# def set_tarea(self, tarea):
+#     pass
 
-    def set_task(self, task):
-        pass
+# def del_tarea(self, id_tarea):
+#     pass
 
-    def del_task(self, task_id):
-        pass
+# def update_tarea(self, id_tarea, detalle):
+#     pass
 
-    def update_task(self, task_id, detalle):
-        pass
+def get_tareas(tipo="all"):
+    consulta = """SELECT id_tarea, tarea, completada
+                    FROM tareas {}
+                    ORDER BY creacion DESC"""
+    where = "WHERE completada = {}"
+    if tipo == "done":
+        consulta = consulta.format(where.format("true"))
+    elif tipo == "pending":
+        consulta = consulta.format(where.format("false"))
+    elif tipo == "all":
+        consulta = consulta.format("")
+    conn = conectar()
+    cur = conn.cursor()
+    cur.execute(consulta)
+    resultado = cur.fetchall()
+    conn.close()
+    print(resultado)
+    return resultado
--- ui.py
+++ ui.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
-
+import datos
 
 class Principal(ttk.Frame):
     def __init__(self, parent):
@@ -9,12 +9,87 @@ class Principal(ttk.Frame):
 
         self.columnconfigure(0, weight=1)   # descripcion
         self.columnconfigure(1, weight=1)   # check
-        self.columnconfigure(2, weight=1)   # eliminar
+        self.columnconfigure(2, weight=1)   # check
         self.columnconfigure(3, weight=0)   # scrollbar
-        self.rowconfigure(0, weight=1)  # titulo
-        self.rowconfigure(1, weight=1)  # tasks
-        self.rowconfigure(2, weight=1)  # botones
 
-        ttk.Label(self, text="TODO app").grid(row=0, column=0, columnspan=3)
-        ttk.Frame(self).grid(row=1, column=0)
-        ttk.Button(self, text="Salir").grid(row=2, column=2, columnspan=2)
+        # self.columnconfigure(3, weight=0)   # scrollbar
+        self.rowconfigure(0, weight=1)  # titulo
+        self.rowconfigure(1, weight=1)  # tipos
+        self.rowconfigure(2, weight=1)  # tareas
+        self.rowconfigure(3, weight=1)  # acciones
+        self.rowconfigure(4, weight=1)  # salir
+
+        ttk.Label(self, text="TODO app").grid(row=0, column=0, columnspan=4)
+        self.set_botonera1()
+        self.set_botonera2()
+        self.set_tabla()
+        ttk.Button(self, text="Salir",
+                   command=self.salir).grid(row=4, column=1, columnspan=2, sticky=tk.E)
+
+        self.cargar_tabla()
+    
+    def cargar_tabla(self, tipo="all"):
+        """tipo es 'all', 'done' o 'pending' """
+        self.vaciar_tabla()
+        registros = datos.get_tareas(tipo)
+        for id_tarea, tarea, completada in registros:
+            self.tabla.insert('', tk.END, values=(tarea, "Si" if completada else "No"))
+        
+    def vaciar_tabla(self):
+        for fila in self.tabla.get_children():
+            self.tabla.delete(fila)
+
+    def set_tabla(self):
+        """crear la tabla"""
+        columnas = ('tarea', 'completada')
+        self.tabla = ttk.Treeview(self, columns=columnas,show='headings',
+                                  selectmode="browse") # sin multi-seleccion
+        self.tabla.grid(row=2, column=0, columnspan=3, sticky=(tk.NSEW))
+        scroll = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.tabla.yview)
+        scroll.grid(row=2, column=3, sticky=tk.NS)
+        self.tabla.configure(yscroll=scroll.set)
+        self.tabla.heading('tarea', text='Descripcion')
+        self.tabla.heading('completada', text='Completada')
+
+    def set_botonera1(self):
+        """crear la botonera para mostrar tareas en la tabla"""
+        botonera1 = ttk.Frame(self)
+        botonera1.grid(row=1, column=0, columnspan=4)
+        botonera1.columnconfigure(0, weight=1)
+        botonera1.columnconfigure(1, weight=1)
+        botonera1.columnconfigure(2, weight=1)
+        ttk.Button(botonera1, text="Mostrar solo pendientes",
+                   command=lambda:self.cargar_tabla("pending")).grid(row=0, column=0)
+        ttk.Button(botonera1, text="Mostrar solo completadas",
+                   command=lambda:self.cargar_tabla("done")).grid(row=0, column=1)
+        ttk.Button(botonera1, text="Mostrar todo",
+                   command=lambda:self.cargar_tabla("all")).grid(row=0, column=2)
+    
+    def set_botonera2(self):
+        """crear botonera de abm de tareas"""
+        botonera2 = ttk.Frame(self)
+        botonera2.grid(row=3, column=0, columnspan=4)
+        botonera2.columnconfigure(0, weight=1)
+        botonera2.columnconfigure(1, weight=1)
+        botonera2.columnconfigure(2, weight=1)
+        ttk.Button(botonera2, text="Completar").grid(row=0, column=0)
+        ttk.Button(botonera2, text="Editar").grid(row=0, column=1)
+        ttk.Button(botonera2, text="Eliminar").grid(row=0, column=2)
+
+    def agregar(self):
+        seleccion = self.tabla.selection()
+        if seleccion:
+            item = self.tabla.item(seleccion[0])
+            id_tarea = item['values'][0]
+            
+        else:
+            messagebox.showinfo(message="Debe seleccionar una tarea primero")
+
+    def completar(self):
+        pass
+
+    def eliminar(self):
+        pass
+
+    def salir(self):
+        self.parent.destroy()
